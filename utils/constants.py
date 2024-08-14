e9982f35f0061c01763cf23918095f4fb22a7176
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2024-08-14 13:58:11 +0100

    feat: Mongo collection for status and incremental loading (#18)
    * feat: Mongo collection for status and incremental loading
    
    * fix: More clear status for extraction
    
    * fix: More clear status for extraction

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 KAFKA_TOPIC = "etl_data"
 KAFKA_TOPIC_TRANSFORMED = "etl_data_transformed"
 KAFKA_SERVER = "localhost:9092"
@@ -7,10 +9,17 @@ OFFSET_RESET = "earliest"
 MONGO_URI = "mongodb://mongo:27017"
 MONGO_DB = "eqtl_database"
 MONGO_COLLECTION = "processed_data"
+MONGO_COLLECTION_STATUS = "pipeline_status"
 FTP_SERVER = "ftp.ebi.ac.uk"
 FTP_USER = "anonymous"
 FTP_PASS = ""
 LOCAL_PATH = "/tmp/eqtl_files"
-SYNC_LOG_PATH = "sync.log"
 FTP_BASE_PATH = "/pub/databases/spot/eQTL/sumstats/"
 # FTP_BASE_PATH = "/pub/databases/spot/eQTL/sumstats/QTS000001/QTD000001/"
+
+
+class SyncStatus(Enum):
+    EXTRACTION_PENDING = "EXTRACTION_PENDING"
+    EXTRACTION_IN_PROGRESS = "EXTRACTION_IN_PROGRESS"
+    EXTRACTION_COMPLETED = "EXTRACTION_COMPLETED"
+    EXTRACTION_FAILED = "EXTRACTION_FAILED"
